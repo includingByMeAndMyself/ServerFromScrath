91e645d003afe34d95b20c0c90a0df75779d6ae1
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-04-24 11:12:33 +0500

    Add project files.

---
+++ ServerFromScrath/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace ServerFromScrath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+            using (var stream = client.GetStream())
+            {
+                using (var reader = new StreamReader(stream))
+                using (var writer = new StreamWriter(stream))
+                {
+                    for (string line = null; line != string.Empty; line = reader.ReadLine())
+                    {
+                        Console.WriteLine(line);
+                    }
+                    writer.WriteLine("Hello from server!");
+                }
+            }
+        }
+    }
+}
